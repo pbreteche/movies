6e66838a2132a35166e32134497949c638bb9dd9
Pierre Bretéché <pbreteche@dawan.fr>
2017-08-22 13:37:57 +0200

    add movie collection display

---
+++ src/app/mock/movies.ts
@@ -0,0 +1,10 @@
+import {Movie} from '../model/movie';
+
+export const MOVIES = [
+  new Movie('Dunkerque', 2017),
+  new Movie('Cars 3', 2017),
+  new Movie('La Planète des Singes - Suprématie', 2017),
+  new Movie('La Ligne verte ', 2000),
+  new Movie('Your Name ', 2016),
+  new Movie(' Tu ne tueras point ', 2016),
+];
