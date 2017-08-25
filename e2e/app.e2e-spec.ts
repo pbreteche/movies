274217bcb3e654c4ed4157e2d5fe78dbb09b91b5
Pierre Bretéché <pbreteche@dawan.fr>
2017-08-25 16:44:40 +0200

    some e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import {element, by, browser} from 'protractor';
 
 describe('movies App', () => {
   let page: AppPage;
@@ -9,6 +10,19 @@ describe('movies App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Movies app');
+  });
+
+  it('should lead to catalog', () => {
+    element(by.cssContainingText('nav a', 'Catalogue')).click();
+    expect(browser.getCurrentUrl()).toMatch('.*//[^\/]*/catalog$');
+  });
+
+  it('should add a movie', () => {
+    browser.get('/catalog/add');
+    element(by.model('movie.title')).sendKeys('Le film de l\'année');
+    element(by.model('movie.year')).sendKeys('2014');
+    element(by.cssContainingText('button', 'Ajouter')).click();
+    expect(element(by.css('h2')).getText()).toEqual('Le film de l\'année');
   });
 });
